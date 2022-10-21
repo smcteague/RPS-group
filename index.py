79d311da86731cf3492282e194d9fb28cbc1a470
ojisamacharles <115052569+ojisamacharles@users.noreply.github.com>
2022-10-21 08:38:45 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,10 @@
+#create a Rock Paper Scissors program.
+
+#FOR COMPUTER
+#Potential solution: create a random number generator.
+random.randitnt(0,2) or (1,3)
+#Each number will be assigned to a value of either Rock, Scissors, or Paper
+# The computer will generate their answer at the same time as we give our answer
+
+#FOR USER
+#Our job will be to choose between three choices. 
