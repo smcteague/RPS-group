8326accb25c2428dd2dc28c9013df5e8eb91400f
Steve McTeague <smcteague@yahoo.com>
2022-10-22 13:38:49 -0500

    grabbed changes

--- index.py
+++ index.py
@@ -29,46 +29,38 @@ combos = {
     }
 }
 
-
-while True:
-    #FOR COMPUTER
-    #Potential solution: create a random number generator.
-    computer_choice_index = random.randint(0,4)
-    computer_choice = choices[computer_choice_index]
-    print(computer_choice)
-    
-
-
-
-    #Each number will be assigned to a value of either Rock, Scissors, or Paper
-    # The computer will generate their answer at the same time as we give our answer
-
-    #FOR USER
-    #Our job will be to choose between three choices. 
-
-    user_choice = input("What is your choice? (Rock / Paper / Scissors/ Lizard/ Spock)? or quit ")
-    user_choice = user_choice.title().strip()
-    
-    
-    print(f"user_choice: {user_choice}")
+#FOR COMPUTER
+#Potential solution: create a random number generator.
+computer_choice_index = random.randint(0,4)
+computer_choice = choices[computer_choice_index]
+print(computer_choice)
    
-   
-    if user_choice == "Quit":
-        break
 
 
-  
 
-    computer_result = ""
+#Each number will be assigned to a value of either Rock, Scissors, or Paper
+# The computer will generate their answer at the same time as we give our answer
 
-    for key1, value1 in combos.items():
-        for key2, value2 in value1.items():
-            if computer_choice == key2 and (user_choice == value2[0] or user_choice == value2[1]):
-                #print(f"\n{key2}, {value2[0]} {value2[1]} computer left")
-                computer_result = key1
-                break
+#FOR USER
+#Our job will be to choose between three choices. 
 
-    print(f"computer {computer_result}")    
+user_choice = input("What is your choice? (Rock / Paper / Scissors/ Lizard/ Spock)? ")
+user_choice = user_choice.title().strip()
+
+print(user_choice)
+
+computer_result = ""
+
+for key1, value1 in combos.items():
+    for key2, value2 in value1.items():
+        if computer_choice == key2 and (user_choice == value2[0] or user_choice == value2[1]):
+            #print(f"\n{key2}, {value2[0]} {value2[1]} computer left")
+            computer_result = key1
+            break
+
+print(f"computer {computer_result}")    
 
 
         
+
+
